63a28d731b3e5f69c8f65b722dff461f742ee16f
Thomas Norden <supermas2000@hotmail.nl>
2024-12-03 11:30:55 +0100

    header animatie toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,131 +1,20 @@
-// const menuKnop = document.querySelector("button")
 
-// function toonMenu () {
-//     const nav = document.querySelector('header > nav');
-//     nav.classList.add('toonMenu');
-// }
-
-// menuButton.addEventListener('click', toonMenu);
-
-
-
-// const sluitButton = document.querySelector('nav > button');
-
-// sluitButton.addEventListener('click', sluitMenu);
-
-// function sluitMenu () {
-//   const nav = document.querySelector('header > nav');
-//   nav.classList.remove('toonMenu');
-// }
-
-
-//Bron: https://codepen.io/shooft/pen/ZEpXmrg gebruikt voor het maken van de bolletjes onder de plaatjes, maar ik heb er zelf nummertjes van gemaakt
-function createCaroCarrousel(carrouselID) {
-	let carrousel = document.querySelector("#"+carrouselID);
-  let carrouselElementsContainer = carrousel.querySelector(":scope > ul");
-	let carrouselElements = carrouselElementsContainer.querySelectorAll("li");
-  let bolletjes = carrousel.querySelectorAll(":scope > nav a");
-	
   
-  /****************/
-	/* DE BOLLETJES */
-	/****************/
-  
-  // de bolletjes activeren
-  function iniBolletjes() {
-    for (bolletje of bolletjes) {
-			// elk bolletje laten luisteren naar kliks
-      bolletje.addEventListener("click", function(e){
-				// als er geklikt wordt
-        
-				// de default-actie (de link volgen) niet uitvoeren
-        // anders verplaatst de hele carrousel naar boven in het scherm
-				e.preventDefault();
 
-				// het nieuwe element opzoeken
-				let newElement = carrousel.querySelector(this.hash);
-        
-        // de linker offset van het nieuwe element bepalen 
-        let newElementOffset = newElement.offsetLeft;
+// Header animatie
+// bron: Thomas Norden & ChatGPT
+let lastScrollY = window.scrollY;
+let header = document.querySelector('header');
 
-        // de carousel naar de berekende positie scrollen
-        carrouselElementsContainer.scrollTo({
-          left: newElementOffset
-        });
-        
-        // nieuwe element current element maken
-		    updateCurrentElement(newElement);
-        
-        // de bolletjes updaten
-		    updateBolletjes(newElement);
-      });
-    }
-	}
-  
-  
-  /*****************/
-	/* START POSITIE */
-	/*****************/
-  
-	// het eerste element en bolletje actief maaken
-  function iniStartPosition() {
-    // eerste element current maken
-    carrouselElements[0].classList.add("current");
-    // eerste bolletje current maken
-		bolletjes[0].classList.add("current");
-		// aan het begin van de container starten
-    carrouselElementsContainer.scrollLeft = 0;
+window.addEventListener('scroll', () => {
+  if (window.scrollY > lastScrollY) {
+    // Scrolling down, hide the header
+    header.classList.remove('header-visible');
+    header.classList.add('header-hidden');
+  } else if (window.scrollY < lastScrollY) {
+    // Scrolling up, show the header
+    header.classList.remove('header-hidden');
+    header.classList.add('header-visible');
   }
-  
-  
-  /*********************/
-	/* ALGEMENE FUNCTIES */
-	/*********************/
-  
-  ////////////////////////////
-	// update current element //
-	function updateCurrentElement(newElement) {
-		// het huidige current element opzoeken
-		let currentElement = carrousel.querySelector(":scope > ul > .current");
-		// de class current verwijderen
-		currentElement.classList.remove("current");
-
-		// aan nieuwe element de class current toevoegen
-		newElement.classList.add("current");
-	}
-
-  
-  //////////////////////
-  // update bolletjes //
-  function updateBolletjes(newElement){
-		// het huidige current bolletje opzoeken
-		let currentBolletje = carrousel.querySelector(":scope > nav .current");
-		// de class current verwijderen
-		currentBolletje.classList.remove("current");
-		
-		// het nieuwe bolletje opzoeken
-    let newBolletje = carrousel.querySelector("a[href='#"+newElement.id+"']");
-		// de class current toevoegen
-		newBolletje.classList.add("current");
-  }
-
-  
-
-	// de bolletjes activeren
-  iniBolletjes();	
-  // de carrousel bij het begin starten
-  iniStartPosition();
-}
-
-
-/************************/
-/* DE CARROUSEL CREËREN */
-/************************/
-
-// nadat de pagina geladen is, de carrousels activeren
-(function() {
-  // hier de id gebruiken van de section in de html
-  createCaroCarrousel("justBolletjes");
-  //je kunt hier ook meerdere carrousellen activeren
-})();
-  
+  lastScrollY = window.scrollY;
+});
